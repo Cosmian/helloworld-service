22741257e69fc8d6063fc62ef925fc523f0f80a6
Franco <francois.colas@cosmian.com>
2023-11-08 13:47:34 +0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    tracing::debug!("GET /");
+
+    "Hello, World!"
+}
